ab93e76f6356b49331c34dce68ef28362cfcbf93
Aathilingam <aathilingam.m@heptagon.in>
2024-06-13 16:11:26 +0530

    import fixes

--- app/Imports/EmployeeImport.php
+++ app/Imports/EmployeeImport.php
@@ -17,13 +17,16 @@ class EmployeeImport implements ToCollection
         foreach ($rows as $row)
         {
             if ($i != 0 && !empty($row[0])) {
-                Employee::create([
-                    'name' => $row[0],
-                    'email' => $row[1],
-                    'contact' => $row[2],
-                    'designation' => $row[3],
-                    'city' => $row[4],
-                ]);
+                $employee = Employee::where('email', $row[1])->first(['id', 'email']);
+                if (empty($employee)) {
+                    Employee::create([
+                        'name' => $row[0],
+                        'email' => $row[1],
+                        'contact' => $row[2],
+                        'designation' => $row[3],
+                        'city' => $row[4],
+                    ]);
+                }
             }
             $i++;
         }
